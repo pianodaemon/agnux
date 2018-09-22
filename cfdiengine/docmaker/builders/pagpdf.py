10012d4344718d3f5ffb8fa00ad188a2381c0584
Bob Ross <pianodaemon@gmail.com>
2018-09-22 14:06:28 -0500

    PAG-COMP : Number canvas has not been included earlier
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- cfdiengine/docmaker/builders/pagpdf.py
+++ cfdiengine/docmaker/builders/pagpdf.py
@@ -261,3 +261,29 @@ class PagPdf(BuilderGen):
         ]))
 
         return table
+
+
+class NumberedCanvas(canvas.Canvas):
+
+    def __init__(self, *args, **kwargs):
+        canvas.Canvas.__init__(self, *args, **kwargs)
+        self._saved_page_states = []
+
+    def showPage(self):
+        self._saved_page_states.append(dict(self.__dict__))
+        self._startPage()
+
+    def save(self):
+        """add page info to each page (page x of y)"""
+        num_pages = len(self._saved_page_states)
+        for state in self._saved_page_states:
+            self.__dict__.update(state)
+            self.draw_page_number(num_pages)
+            canvas.Canvas.showPage(self)
+        canvas.Canvas.save(self)
+
+    def draw_page_number(self, page_count):
+        width, height = letter
+        self.setFont("Helvetica", 7)
+        self.drawCentredString(width / 2.0, 0.65*cm,
+            "Pagina %d de %d" % (self._pageNumber, page_count))
