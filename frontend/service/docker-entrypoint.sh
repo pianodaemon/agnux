9cb263e1fbe79486778a601f3c9ac7c9966f062a
Debian <admin@ip-172-31-45-146.us-east-2.compute.internal>
2018-11-12 07:29:10 +0000

    IaC : Fixed typo
    Signed-off-by: Debian <admin@ip-172-31-45-146.us-east-2.compute.internal>

--- frontend/service/docker-entrypoint.sh
+++ frontend/service/docker-entrypoint.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
 
-BASE_DIR="$PWD"
-GLASS="$BASE_DIR/glassfish3"
-ASADMIN=$GLASS/bin/asadmin
+if [ "$1" = 'asadmin' ]; then
 
-$ASADMIN start-domain
-$ASADMIN -u admin deploy $1
-$ASADMIN stop-domain
-$ASADMIN start-domain --verbose
+    asadmin start-domain
+    asadmin -u admin deploy agnux.war
+    asadmin stop-domain
+
+    if [ "$AS_ADMIN_PASSWORD" ]; then
+        echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
+        echo "AS_ADMIN_NEWPASSWORD=${AS_ADMIN_PASSWORD}" >> /tmp/glassfishpwd
+
+        # Change the admin password
+        asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1
+
+        if [ "$AS_ADMIN_ENABLE_SECURE" ]; then
+            echo "AS_ADMIN_PASSWORD=${AS_ADMIN_PASSWORD}" > /tmp/glassfishpwd
+            asadmin start-domain
+
+            asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin
+
+            # Call asadmin at least once to establish a trust with the
+            # self-signed certificate for the admin API.
+            # asadmin --interactive=false version
+
+            asadmin stop-domain
+        fi
+
+        rm /tmp/glassfishpwd
+    fi
+
+    exec "$@"
+fi
+
+exec "$@"
