5897249d109f021db4c41d8afc0a837f5f0bd890
Edward Nygma <j4nusx@yahoo.com>
2018-11-19 17:12:40 -0800

    engine : FatalError exception
    Exception that shows a color red message
    
    Signed-off-by: Edward Nygma <j4nusx@yahoo.com>

--- fiscal/service/misc/magic.py
+++ fiscal/service/misc/magic.py
@@ -1,8 +1,26 @@
+import enum
+
+
+class UMT(enum.Enum):
+    """
+    Unix terminal format codes
+    """
+    NORMAL        = '\033[0m'
+    BOLD          = '\033[1m'
+    DIM           = '\033[2m'
+    UNDERLINE     = '\033[4m'
+    REVERSE       = '\033[7m'
+    STRIKETHROUGH = '\033[9m'
+    RED           = '\033[31m'
+    YELLOW        = '\033[33m'
 
 
 def attached_var(name, value):
-    """Decorator to mimic static variables"""
+    """
+    Decorator to mimic static variables
+    """
     def decorate(func):
         setattr(func, name, value)
         return func
-    return decorat
+
+    return decorate
