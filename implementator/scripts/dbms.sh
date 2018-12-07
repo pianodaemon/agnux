5ea89af5bfc46e39c9dd871a73e6eccfa62368f0
J4nusx's Universe <j4nusx@yahoo.com>
2018-12-07 00:58:43 -0600

    IaC : Added timezone to dbms container initialization
    Signed-off-by: J4nusx's Universe <j4nusx@yahoo.com>

--- implementator/scripts/dbms.sh
+++ implementator/scripts/dbms.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 PG_SQL_IMG="postgres:11.1-alpine"
-docker run --name dbms -e POSTGRES_PASSWORD=1234qwer -p 5432:5432 -d $PG_SQL_IMG
+docker run --name dbms \
+-e POSTGRES_PASSWORD=1234qwer \
+-e TZ=`cat /etc/timezone` \
+-p 5432:5432 -d $PG_SQL_IMG
